4d9f750e9f26a695b639c938756a78d25dfb116d
khalidofislam@gmail.com <khalidofislam@gmail.com@755ae2e4-2abe-b3b6-2940-64e09b8d6a63>
2013-05-06 02:05:58 +0000

    Pausing on contact

--- castle-draculex/Assets/Follower.js
+++ castle-draculex/Assets/Follower.js
@@ -5,6 +5,7 @@ var leader : Transform;
 
 var speed : float = 5; // The speed of the follower 
 
+var colliding = false;
 
  
 function Start () {
@@ -13,22 +14,35 @@ function Start () {
 
 function FixedUpdate(){ 
 	
-	var distance = Vector3.Distance(leader.position, transform.position);
 	
-	if(distance < 7) {
-	   	transform.LookAt(leader);
-	   	speed = 7;  
-   	}
-   	else {
-   		if (Random.Range(0, 100) <= 1)
-   		{
-			transform.localEulerAngles.x = Random.Range(30, 360);
-		}
-		speed = 3;
-   	}
-
-   	rigidbody.MovePosition(rigidbody.position + transform.forward * speed * Time.deltaTime);
-
+	if (!colliding) {
+		var distance = Vector3.Distance(leader.position, transform.position);
+		
+		if(distance < 7) {
+		   	transform.LookAt(leader);
+		   	speed = 7;  
+	   	}
+	   	else {
+	   		if (Random.Range(0, 100) <= 1)
+	   		{
+				transform.localEulerAngles.x = Random.Range(30, 360);
+			}
+			speed = 3;
+	   	}
+	
+	   	rigidbody.MovePosition(rigidbody.position + transform.forward * speed * Time.deltaTime);
+	}
 }
 
+function OnTriggerEnter(other: Collider)
+{
+	Debug.Log("Collision");
+	colliding = true;
+	
+}
 
+function OnTriggerExit(other: Collider)
+{
+	Debug.Log("End Collision");
+	colliding = false;
+}
