c1ed8e72a9c66f8523abb7a43f526e9063c56026
echoone10 <echoone10@755ae2e4-2abe-b3b6-2940-64e09b8d6a63>
2013-05-12 21:12:38 +0000

    Added manual level, level loader is disabled.

--- castle-draculex/Assets/CameraFollow.cs
+++ castle-draculex/Assets/CameraFollow.cs
@@ -6,6 +6,8 @@ public class CameraFollow : MonoBehaviour {
 	public Transform target;
 	Vector3 offset  = new Vector3 (0, 0, -1000);
 	
+
+	
 	void Update () {
 		transform.position =  target.position + offset;
 	}
