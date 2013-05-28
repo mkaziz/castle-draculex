13dc511da662101bd5d4f45bb5b4ea3cbf36b465
khalidofislam@gmail.com <khalidofislam@gmail.com@755ae2e4-2abe-b3b6-2940-64e09b8d6a63>
2013-05-28 10:17:46 +0000

    Added healthbar

---
+++ CastleDraculEX_v2/Assets/Health/HealthBarScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class HealthBarScript : MonoBehaviour {
+
+	public float barDisplay; //current progress
+    public Vector2 pos = new Vector2(20,40);
+    public Vector2 size = new Vector2(60,20);
+    public Texture2D emptyTex;
+    public Texture2D fullTex;
+	
+	void Start() {
+		
+		emptyTex.wrapMode = TextureWrapMode.Repeat;
+		fullTex.wrapMode = TextureWrapMode.Repeat;
+		barDisplay = 300;
+	}
+ 
+    void OnGUI() {
+		GUI.DrawTexture(new Rect(pos.x, pos.y, pos.x+size.x, pos.y+size.y), emptyTex, ScaleMode.StretchToFill, false, 10.0F);
+		GUI.DrawTexture(new Rect(pos.x, pos.y, pos.x+barDisplay, pos.y+size.y), fullTex, ScaleMode.StretchToFill, false, 10.0F);
+
+    }
+ 
+    void Update() {
+       //for this example, the bar display is linked to the current time,
+       //however you would set this value based on your desired display
+       //eg, the loading progress, the player's health, or whatever.
+       //barDisplay = 100;
+   		Component playerHealthScript = GameObject.FindWithTag("Player").GetComponent("PlayerControl");
+		PlayerControl pc = (PlayerControl) playerHealthScript;
+		barDisplay = pc.Health * 3;
+    }
+}
