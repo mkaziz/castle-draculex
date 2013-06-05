91168eeea9dd20d2fd3513e4cdd25e844789f3ac
echoone10 <echoone10@755ae2e4-2abe-b3b6-2940-64e09b8d6a63>
2013-06-05 20:42:19 +0000

    Pushing changes from class - chasm should be working.

--- CastleDraculEX_v2/Assets/WorldObjects/Chasm/Chasm.cs
+++ CastleDraculEX_v2/Assets/WorldObjects/Chasm/Chasm.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class Chasm : MonoBehaviour {
 	
 	void OnTriggerEnter(Collider other) {
-		Debug.Log("Chasm collision");
+		
 		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		//Debug.Log("Is " + player.name + " == " + other.name);
 		if (other.name == player.name) {
-			PlayerControl pc = (PlayerControl) player.GetComponent("PlayerControl");
+			//Debug.Log ("Found player!");
+			Player pc = (Player) player.GetComponent("Player");
 			pc.Health = 0;
 		}
 	}
