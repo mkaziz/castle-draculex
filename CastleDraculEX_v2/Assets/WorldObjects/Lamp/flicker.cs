160790c6dc931b09c9e56c87dba1579a95d8b67c
echoone10 <echoone10@755ae2e4-2abe-b3b6-2940-64e09b8d6a63>
2013-06-10 21:48:02 +0000

    Fixed up level 2. Now on to level 3.

--- CastleDraculEX_v2/Assets/WorldObjects/Lamp/flicker.cs
+++ CastleDraculEX_v2/Assets/WorldObjects/Lamp/flicker.cs
@@ -3,46 +3,57 @@ using System.Collections;
 
 public class flicker : MonoBehaviour {
 
-	float minOnFlickerSpeed = 0.2f;
-	float maxOnFlickerSpeed = 0.5f;
-	float minOffFlickerSpeed = 0.01f;
-	float maxOffFlickerSpeed = 0.05f;
+	float minOnFlickerSpeed = 1.0f;
+	float maxOnFlickerSpeed = 1.3f;
+	float minOffFlickerSpeed = 0.1f;
+	float maxOffFlickerSpeed = 0.2f;
 	
 	float off_screen_distance = 30;
 	
 	GameObject player;
 	
 	void Start() {
-		
 		gameObject.light.enabled = true;	
-		
-		player = GameObject.FindWithTag("Player");
-		
-	}
-	float dist = 0;
-	bool light_off = true;
-	void Update () {
-		dist = Vector3.Distance(player.transform.position, gameObject.transform.position);
-		if (light_off && dist < off_screen_distance) {
-			StartCoroutine(initiateflicker());
-		}
+		player = GameObject.FindWithTag("Player");	
+		StartCoroutine(initiateflicker());
 	}
 	
+	float dist = 0;
+	bool light_on = false;
+	void Update () {
+		//dist = Vector3.Distance(player.transform.position, gameObject.transform.position);
+		//if (!light_on && onScreen ()) {
+		//	StartCoroutine(initiateflicker());
+		//	light_on = true;
+		//}
+	}
+	
+	bool onScreen() {
+		return dist < off_screen_distance;	
+	}
+	
+	float wait_time;
+	
 	IEnumerator initiateflicker () {
-		if(dist < off_screen_distance){ //optimize - if off screen, don't turn it on
+		//dist = Vector3.Distance(player.transform.position, gameObject.transform.position);
+		//if(dist < off_screen_distance){ //flicker if on screen
 			gameObject.light.enabled = true;
-			float n1 = Random.Range(minOnFlickerSpeed, maxOnFlickerSpeed );
-			yield return new WaitForSeconds(n1);
-			gameObject.light.enabled = false;
+			float wait_time = Random.Range(minOnFlickerSpeed, maxOnFlickerSpeed );
+			yield return new WaitForSeconds(wait_time);
 			
-			n1 = Random.Range(minOffFlickerSpeed, maxOffFlickerSpeed );
-			yield return new WaitForSeconds(n1);
-		}
-		else {
-			yield return new WaitForSeconds(1000);
-			light_off = false;
-		}
+			gameObject.light.enabled = false;
+			wait_time = Random.Range(minOffFlickerSpeed, maxOffFlickerSpeed );
+			yield return new WaitForSeconds(wait_time);
+			
 			StartCoroutine(initiateflicker());
+		/*}
+		else { //turn it off if off screen
+			gameObject.light.enabled = false;
+			//new WaitForSeconds(1000); 
+			light_on = false;
+			yield break;
+		}*/
+			
 
 	}
 	
