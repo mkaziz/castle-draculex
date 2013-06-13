6f0d870c29dba7627be81f723466a8fc41b25b69
echoone10 <echoone10@755ae2e4-2abe-b3b6-2940-64e09b8d6a63>
2013-06-13 04:05:39 +0000

    Pushing win scene and some fixes.

--- trunk/CastleDraculEX_v2/Assets/Levels/End/TheEnd.cs
+++ trunk/CastleDraculEX_v2/Assets/Levels/End/TheEnd.cs
@@ -5,8 +5,8 @@ public class TheEnd : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Player") {
-			//Application.LoadLevel("win");
-			Debug.Log("You win!");
+			Application.LoadLevel("win");
+			//Debug.Log("You win!");
 		}
 	}
 }
