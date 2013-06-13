3fd3f900f23048a57dfb74da1b5d034f91e34541
echoone10 <echoone10@755ae2e4-2abe-b3b6-2940-64e09b8d6a63>
2013-06-13 03:23:31 +0000

    Again, forgot to add assets.

---
+++ trunk/CastleDraculEX_v2/Assets/Levels/End/TheEnd.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class TheEnd : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			//Application.LoadLevel("win");
+			Debug.Log("You win!");
+		}
+	}
+}
